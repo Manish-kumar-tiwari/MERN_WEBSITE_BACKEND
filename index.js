d00f1cab1985cbca68cc344be7cc4f86047efc4a
Manish <mt5155614@gmail.com>
2024-03-03 13:24:20 +0530

    third commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const connectDb=require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const corsOptions = {
-        origin: ["http://localhost:5173","https://mern-website-frontend.vercel.app/"],
+        origin: ["http://localhost:5173","https://mern-website-frontend.vercel.app"],
         methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
         credentials: true,
       };
